f549c7ba84761d99e23cc721161e1b57766190e3
Adam Kariv <adam.kariv@gmail.com>
2023-02-20 18:30:39 +0200

    Remove datacity reference

--- taxonomies/trees/process.py
+++ taxonomies/trees/process.py
@@ -1,7 +1,5 @@
 from dgp.core.base_enricher import enrichments_flows
 
-from datacity_server.processors import \
-    FilterEmptyFields, AddressFixer, GeoCoder, GeoProjection
 
 
 def flows(config, context):
