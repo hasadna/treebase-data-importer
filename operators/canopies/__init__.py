73cae2111e2aaed7d0019c5ea44f4b074d9a4156
Adam Kariv <adam.kariv@gmail.com>
2023-02-24 14:48:11 +0200

    Load and process mapi canopies

---
+++ operators/canopies/__init__.py
@@ -0,0 +1,90 @@
+import os
+import requests
+import shutil
+import json
+
+import fiona
+from pyproj import Transformer
+from shapely.ops import transform, unary_union
+from shapely.geometry import shape, mapping
+
+from treebase.s3_utils import S3Utils
+
+def main():
+    print('PROCESSING CANOPIES')
+
+    s3 = S3Utils()
+
+    canopies_gdb_file = 'canopies.gdb.zip'
+    with s3.get_or_create('processed/canopies/canopies.gdb.zip', canopies_gdb_file) as fn:
+        if fn:
+            print('### Downloading from data.gov.il ###')
+            dataset = requests.get('https://data.gov.il/api/action/package_search?q=nationalcanopytrees').json()['result']['results'][0]
+            resource = dataset['resources'][0]['url']
+            resource = resource.replace('/e.', '/')
+            with open(fn, 'wb') as outfile:
+                r = requests.get(resource, headers={'User-Agent': 'datagov-external-client'}, stream=True)
+                if r.status_code == 200:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, outfile)
+
+    geojson_file = 'canopies.geojson'
+    with s3.get_or_create('processed/canopies/canopies.geojson', geojson_file) as fn:
+        if fn:
+            print('### Converting to GeoJSON ###')
+            layername = 'Alltrees'
+            with open(geojson_file, 'w') as outfile:
+                with fiona.open(fn, layername=layername) as collection:
+                    print('CRS', collection.crs)
+                    transformer = None
+                    if collection.crs['init'] != 'epsg:4326':
+                        transformer = Transformer.from_crs(collection.crs['init'], 'epsg:4326', always_xy=True)
+                    outfile.write('{"type": "FeatureCollection", "features": [')
+                    first = True
+                        
+                    for item in collection.filter():
+                        if item['geometry'] is None:
+                            continue
+                        geometry = shape(item['geometry'])
+                        if transformer is not None:
+                            geometry = transform(transformer.transform, shape(geometry))
+                        if first:
+                            first = False
+                        else:
+                            outfile.write(',')
+                        area = item['properties']['Shape_Area']
+                        geometry = mapping(geometry)
+                        outfile.write(json.dumps(dict(
+                            type='Feature',
+                            properties={'area': area},
+                            geometry=geometry,
+                        )) + '\n')
+                outfile.write(']}')
+
+    filtered_geojson_file = 'extracted_trees.geojson'
+    with s3.get_or_create('processed/canopies/extracted_trees.geojson', filtered_geojson_file) as fn:
+        if fn:
+            import dataflows as DF
+            from .distance_to_road import distance_to_road
+
+            MIN_AREA = 4
+            MAX_AREA = 200
+
+            print('### Filtering by area, Calculating distance to road ###')
+            DF.Flow(
+                DF.load(geojson_file),
+                DF.filter_rows(lambda r: r['area'] > MIN_AREA and r['area'] < MAX_AREA),
+                DF.add_field('coords', 'object', lambda r: mapping(shape(r['__geometry']).centroid)),
+                DF.select_fields(['coords', 'area']),
+                DF.set_type('coords', type='geojson', transform=lambda v: json.dumps(v)),
+                distance_to_road(),
+                DF.update_resource(-1, name='extracted_trees', path='extracted_trees.geojson'),
+                DF.dump_to_path('.', format='geojson'),
+            ).process()
+
+
+def operator(*_):
+    main()
+
+if __name__ == "__main__":
+    main()
---
+++ operators/canopies/distance_to_road.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+import shutil
+import json
+
+from shapely.ops import transform
+from pyproj import Transformer
+from shapely.geometry import shape, Point
+import fiona
+import requests
+
+import dataflows as DF
+
+
+SEARCH_RADIUS = 20
+MAX_DISTANCE = 10
+
+# Tel Aviv Center
+center_x, center_y = 34.75, 32.05
+crs = f'+proj=tmerc +lat_0={center_y} +lon_0={center_x} +k_0=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs'
+transformer = Transformer.from_crs('EPSG:4326', crs, always_xy=True)
+inv_transformer = Transformer.from_crs(crs, 'EPSG:4326', always_xy=True)
+diff_x, diff_y = inv_transformer.transform(SEARCH_RADIUS, SEARCH_RADIUS)
+diff_x -= center_x
+diff_y -= center_y
+print('DIFFS', diff_x, diff_y)
+
+def download_gpkg():
+    GPKG_FILE = Path(__file__).with_name('roads.gpkg')
+    GPKG_URL = 'https://s3.eu-west-2.wasabisys.com/opentreebase-public/geo/roads.gpkg'
+    if not GPKG_FILE.exists():
+        print('Downloading', GPKG_URL)
+        with requests.get(GPKG_URL, stream=True) as r:
+            with GPKG_FILE.open('wb') as f:
+                shutil.copyfileobj(r.raw, f)
+    return GPKG_FILE
+
+
+def feature_cache(gpkg: fiona.Collection, row):
+    lon_deg, lat_deg = json.loads(row['__geometry'])['coordinates']
+    crs = f'+proj=tmerc +lat_0={lat_deg} +lon_0={lon_deg} +k_0=1 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs'
+    transformer = Transformer.from_crs('EPSG:4326', crs, always_xy=True)
+    # ids = set()
+    features = []
+    bbox = (lon_deg-diff_x, lat_deg-diff_y, lon_deg+diff_x, lat_deg+diff_y)
+    # print('QUERYING FEATURES...', lon_deg, lat_deg, bbox)
+    features = [
+        transform(transformer.transform, shape(f['geometry']))
+        for _, f in gpkg.items(bbox=bbox)
+    ]
+    return features
+
+
+def distance_to_road():
+
+    gpkg = fiona.open(str(download_gpkg()), layer='gis_osm_roads_free_1')
+    origin = Point(0, 0)
+
+    def func(rows):
+        for row in rows:
+            features = feature_cache(gpkg, row)
+            minimum = None
+            if len(features) > 0:
+                for geom in features:
+                    distance = origin.distance(geom)
+                    if minimum is None or distance < minimum:
+                        minimum = distance
+
+                if minimum is not None and minimum < MAX_DISTANCE:
+                    row['distance_to_road'] = minimum
+                    yield row
+
+    return DF.Flow(
+        DF.add_field('distance_to_road', 'number'),
+        func
+    )
