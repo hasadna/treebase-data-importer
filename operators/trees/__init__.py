f666a727f40a0ff04f566710ebeb56842e2b262e
Adam Kariv <adam.kariv@gmail.com>
2023-03-16 18:29:42 +0200

    Clean up genus using table

--- operators/trees/__init__.py
+++ operators/trees/__init__.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 import shutil
 import dataflows as DF
+import re
 
 # from dataflows_ckan import dump_to_ckan
 from rtree import index
 from geopy.distance import distance
 from openlocationcode import openlocationcode as olc
+
+from dataflows_airtable import load_from_airtable
+
+from thefuzz.process import extractOne
 
 from treebase.mapbox_utils import run_tippecanoe, upload_tileset
 from treebase.log import logger
@@ -66,22 +71,68 @@ def match_index(idx: index.Index, clusters, matched):
         func,
     )
 
+def clean_genus():
+
+    WORDS = re.compile(r'[a-zA-Zא-ת]+')
+    table = DF.Flow(
+        load_from_airtable('appHaG591cVK21CRl', 'Genus', 'Grid view', 'env://AIRTABLE_API_TOKEN'),
+        DF.add_field('attributes-genus-clean-en', 'string', lambda r: r['id']),
+        DF.add_field('attributes-genus-clean-he', 'string', lambda r: r['name']),
+        DF.select_fields(['attributes-genus-clean-en', 'attributes-genus-clean-he']),
+    ).results()[0][0]
+    options = dict([
+        (r['attributes-genus-clean-en'].lower(), r)
+        for r in table
+    ] + [
+        (r['attributes-genus-clean-he'], r)
+        for r in table
+    ] + [
+        (r['attributes-genus-clean-en'] + ' ' + r['attributes-genus-clean-he'], r)
+        for r in table
+    ])
+    option_keys = list(options.keys())
+
+    def func(rows):
+        for row in rows:
+            genus = row.get('attributes-genus')
+            if genus:
+                genus = ' '.join(WORDS.findall(genus.lower()))
+                found = extractOne(genus, option_keys, score_cutoff=80)
+                if found:
+                    best, _ = found
+                    option = options[best]
+                    row.update(option)
+            yield row
+
+    return DF.Flow(
+        DF.add_field('attributes-genus-clean-en', 'string'),
+        DF.add_field('attributes-genus-clean-he', 'string'),
+        func
+    )
+
+
 def main(local=False):
     logger.info('PROCESSING TREE DATASET')
     shutil.rmtree('.checkpoints', ignore_errors=True, onerror=None)
 
-    print('### Loading data and geo-indexing ###')
-    idx = index.Index()
+    print('### Loading data and processing ###')
     DF.Flow(
         DF.load('env://DATASETS_DATABASE_URL', format='sql', table='trees', query='SELECT * FROM trees'),
-        # dump_to_ckan(host, api_key, owner_org, overwrite_existing_data=True, push_to_datastore=False),
         DF.update_resource(-1, name='trees', path='trees.csv'),
         DF.add_field('coords', 'geopoint', lambda r: [float(r['location-x']), float(r['location-y'])]),
+        clean_genus(),
+        DF.checkpoint('tree-processing'),
+    ).process()
+
+    print('### geo-indexing ###')
+    idx = index.Index()
+    DF.Flow(
+        DF.checkpoint('tree-processing'),
         DF.add_field('meta-collection-type-idx', 'integer', lambda r: 1 if r['meta-collection-type'] == 'חישה מרחוק' else 0),
         DF.sort_rows('{meta-collection-type-idx}'),
         DF.delete_fields(['meta-collection-type-idx']),
         spatial_index(idx),
-        DF.checkpoint('tree-processing'),
+        DF.checkpoint('tree-deduping'),
     ).process()
 
     print('### DeDuping and assigning TreeId ###')
@@ -89,7 +140,7 @@ def main(local=False):
     clusters = dict()
     matched = dict()
     DF.Flow(
-        DF.checkpoint('tree-processing'),
+        DF.checkpoint('tree-deduping'),
         match_index(idx, clusters, matched),
         DF.checkpoint('tree-processing-clusters')
     ).process()
@@ -100,13 +151,17 @@ def main(local=False):
     DF.Flow(
         DF.checkpoint('tree-processing-clusters'),
         DF.dump_to_path('trees-full', format='csv'),
-        # DF.dump_to_path('trees-full', format='geojson'),
-        DF.select_fields(['coords', 'meta-tree-id']),
+        DF.select_fields(['coords', 'meta-tree-id', 'meta-source']),
+        DF.join_with_self('trees', ['meta-tree-id'], fields={
+            'tree-id': dict(name='meta-tree-id'),
+            'coords': None,
+            'sources': dict(name='meta-source', aggregate='set'),
+        }),
+        DF.set_type('sources', type='string', transform=lambda v: ', '.join(v)),
         DF.dump_to_path('trees-compact', format='geojson'),
     ).process()
 
     s3 = S3Utils()
-    # s3.upload('trees-full/trees.geojson', 'processed/trees/trees.geojson')
     s3.upload('trees-full/trees.csv', 'processed/trees/trees.csv')
 
     print('### Uploading to MapBox ###')
